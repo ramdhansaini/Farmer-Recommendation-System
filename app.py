9f114010f3ff08c4e79115ff7b0682fffa1dd3e7
Ramdhan Saini <sainiramdhan286@gmail.com>
2026-07-05 18:48:05 +0530

    Update

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(BASE_DIR, "models", "random_forest_model.pkl")
 scaler_path = os.path.join(BASE_DIR, "models", "standard_scaler.pkl")
 encoder_path = os.path.join(BASE_DIR, "models", "label_encoder.pkl")
+ferti_model_path = os.path.join(BASE_DIR, "models", "fertilizer_model.pkl")
+ferti_scaler_path = os.path.join(BASE_DIR, "models", "scaler_fertilizer.pkl")
+ferti_encoder_path = os.path.join(BASE_DIR, "models", "label_encoder_fertilizer.pkl")
 
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
@@ -14,17 +17,27 @@ with open(scaler_path, 'rb') as f:
     scaler = pickle.load(f)
 with open(encoder_path, 'rb') as f:
     encoder = pickle.load(f)
+with open(ferti_model_path, 'rb') as f:
+    ferti_model = pickle.load(f)
+with open(ferti_scaler_path, 'rb') as f:
+    ferti_scaler = pickle.load(f)
+with open(ferti_encoder_path, 'rb') as f:
+    ferti_encoder = pickle.load(f)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/crop_recommendation')
+@app.route('/crop_recommendation', methods=['GET', 'POST'])
 def crop_recommendation():
+    if request.method == 'POST':
+        return predict_crop()
     return render_template('crop_recommendation.html')
 
-@app.route('/fertilizer_recommendation')
+@app.route('/fertilizer_recommendation', methods=['GET', 'POST'])
 def fertilizer_recommendation():
+    if request.method == 'POST':
+        return predict_fertilizer()
     return render_template('fertilizer_recommendation.html')
 
 @app.route('/predict_crop', methods=['POST'])
@@ -59,11 +72,11 @@ def predict_fertilizer():
     
 
     input_data = np.array([[N, P, K, crop_type, temperature, moisture, ph, rainfall, carbon_content, soil_type]])
-    input_data_scaled = scaler.transform(input_data)
-    prediction_encoded = model.predict(input_data_scaled)
-    prediction = encoder.inverse_transform(prediction_encoded)
+    input_data_scaled = ferti_scaler.transform(input_data)
+    prediction_encoded = ferti_model.predict(input_data_scaled)
+    prediction = ferti_encoder.inverse_transform(prediction_encoded)
 
-    return render_template('result.html', prediction=prediction[0])
+    return render_template('result2.html', prediction=prediction[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
